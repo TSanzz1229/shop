e1d78bbc0748d219d482aa231e58465ee55212be
TSanzz1229 <119602416+TSanzz1229@users.noreply.github.com>
2023-09-25 20:38:28 +0700

    First commit

---
+++ Areas/Admin/Controllers/CategoryCtl.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Phone.Data.Repositories;
+using Phone.Data.ViewModels;
+
+namespace PhoneShopnet.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class CategoryCtl : Controller
+	{
+		private IUnit _unit;
+		public CategoryCtl(IUnit unit) 
+		{
+			_unit = unit;
+		}
+		public IActionResult Index()
+		{
+			CategoryVM categoryVM = new CategoryVM();
+			categoryVM.Categories = _unit.Category.GetAll();
+			return View(categoryVM);
+		}
+
+		[HttpGet]
+		public IActionResult CreateUpdate (int? ID)
+		{
+			CategoryVM vm = new CategoryVM();
+			if(ID == null || ID == 0)
+			{
+				return View(vm);
+			}
+			else
+			{
+				vm.Category = _unit.Category.GetT(x => x.ID == ID);
+				if(vm.Category == null)
+				{
+					return NotFound();
+				}
+				else
+				{
+					return View(vm);
+				}
+			}
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult CreateUpdate (CategoryVM vm)
+		{
+			if(ModelState.IsValid)
+			{
+				if(vm.Category.ID == 0)
+				{
+					_unit.Category.Add(vm.Category);
+					TempData["Success"] = "Category create success";
+                }
+				else
+				{
+                    _unit.Category.Update(vm.Category);
+                    TempData["Success"] = "Category update success";
+                }
+				_unit.Save();
+				return RedirectToAction("Index");
+			}
+            return RedirectToAction("Index");
+        }
+
+		[HttpGet]
+		public IActionResult Delete (int? Id)
+		{
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+			var category = _unit.Category.GetT(x => x.ID == Id);
+			if(category == null)
+			{
+				return NotFound();
+			}
+			return View(category);
+        }
+
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public IActionResult DeleteData (int? Id)
+		{
+			var category = _unit.Category.GetT(x => x.ID == Id);
+			if(category == null)
+			{
+				return NotFound();
+			}
+			_unit.Category.Delete(category);
+			_unit.Save();
+			TempData["success"] = "Category delete done";
+			return RedirectToAction("Index");
+		}
+	}
+}
---
+++ Areas/Admin/Controllers/ProductCtl.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Phone.Data.Repositories;
+using Phone.Models;
+using System.Net;
+
+namespace PhoneShopnet.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductCtl : Controller
+    {
+        private IUnit _unit;
+        public ProductCtl(IUnit unit)
+        {
+            _unit = unit;
+        }
+
+    }
+}
